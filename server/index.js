20d456682748125a3a71a847c3b8231d0bf8e957
Monica Choe <monicchoe@gmail.com>
2017-07-24 15:02:52 -0400

    setting up oauth 2

--- server/index.js
+++ server/index.js
@@ -3,28 +3,61 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 
+// Parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Logging Middleware
 app.use(morgan('dev'));
 
+// Serving up static files 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+
+// Session Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Initializing Passport [passport is authentication middleware]
+app.use(passport.initiailize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
 
 // Back-end routes
 app.use('/api', require('./routes'));
 
+// All other routes
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+// Error-handling
 app.use(function (err, req, res, next) {
   console.error(err);
   console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
 
+// Starting server
 app.listen(3000, function () {
   console.log("Knock, knock");
   console.log("Who's there?");
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 router.use('/users', require('./users')); // matches all requests to /api/users/
 router.use('/puppies', require('./puppies')); // matches all requests to  /api/puppies/
+router.use('/auth', require('./auth'));
 
 // Sloths?!?!
 router.use(function (req, res, next) {
